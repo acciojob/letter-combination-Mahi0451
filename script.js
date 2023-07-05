496e1996d75958cdc8747115e684966bf04a0135
Manoj Saini <manojsaini513@gmail.com>
2023-07-05 15:14:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 function letterCombinations(input) {
   //Complete the function
+ 
 	let table={
-		"2":"abc",
-		"3":"def",
-		"4":"ghi",
-		"5":"jkl",
-		"6":"mno",
-		"7":"pqrs",
-		"8":"tuv",
-	    "9":"wxyz"
+		"2": "abc",
+		"3": "def",
+		"4": "ghi",
+		"5": "jkl",
+		"6": "mno",
+		"7": "pqrs",
+		"8": "tuv",
+		"9": "wxyz"
 	}
-let solution=[];
-
-	function solve(currindex,calculatedString){
-		if(input.lenght<=currindex){
+ 
+	let solution=[];
+ 
+	function solve(currentIndex, calculatedString){
+		if(input.length<= currentIndex){
 			solution.push(calculatedString);
 			return;
 		}
-		let currchar=input[currindex];
-
-		for(let i=0;i<table[currchar].length;i++){
-			solve(currindex+1,calculatedString+table[currchar][i]);
-			
+ 
+		let currentChar = input[currentIndex];
+		for(let i=0;i< table[currentChar].length;i++){
+			solve(currentIndex +1, calculatedString + table[currentChar][i]);
 		}
 	}
-	solve(0,"");
-	return solution;
-	
+	solve(0, "");
+ 
+	return(solution);
+ 
 	
 }
-
+ 
 module.exports = letterCombinations;
